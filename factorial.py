a147a93bc71c170d990b3f0f0188ccb305c901a2
Grecia Corona <grecia.corona@gmail.com>
2020-11-02 13:09:47 -0600

    function run added

--- factorial.py
+++ factorial.py
@@ -1,25 +1,27 @@
 # Python program to find the factorial of a number
 # using "recursion" and "for loop"
 
-def fact(n):
+def recursive_factorial(n):
     if n == 0:
         return 1
-    elif n < 0:
-        return f'Sorry, factorial does not exist for negative numbers'
-    return n*fact(n-1)
+    return n*recursive_factorial(n-1)
 
 
-def factorial(n):
-    factori = 1
+def loop_factorial(n):
+    fact = 1
     if n == 0:
         return 1
-    elif n < 0:
-        return f'Sorry, factorial does not exist for negative numbers'
     for i in range (1, n+1):
-        factori = factori*i
-    return factori
+        fact = fact*i
+    return fact
 
+def run():
+    n = int(input('Enter a number: '))
+    if n < 0:
+        print('Sorry, factorial does not exist for negative numbers')
+    else: 
+        print(f'Using recursion: {recursive_factorial(n)}')
+        print(f'Using for loop: {loop_factorial(n)}')
 
-n = int(input('Enter a number: '))
-print(f'Using recursion: {fact(n)}')
-print(f'Using for loop: {factorial(n)}')
+if __name__ == "__main__":
+    run()
